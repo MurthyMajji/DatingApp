014f6564511bcef6a2b03ea1b742f8b496125b1a
Naveen-Murthy <naaveen2@gmail.com>
2026-05-24 13:16:52 +0530

    Updated Azure-GitHub Workflow

--- client/src/app/pages/home/home.ts
+++ client/src/app/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Register } from '../../layout/register/register';
+import { AccountServices } from '../../core/services/account-services';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,7 @@ import { Register } from '../../layout/register/register';
   styleUrl: './home.css',
 })
 export class Home {
+  protected accountService = inject(AccountServices);
   protected registerMode = signal(false);
 
   showRegister(value: boolean) {
